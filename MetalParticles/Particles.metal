20bc6abab7e58c9a59bc7e708fe956ca7ea8181e
Simon Gladman <sgladman@gmail.com>
2015-01-31 07:48:28 +0000

    Slightly failed experiment to determine whether a given number is prime using a Metal shader. Sadly, Swift is quicker than Metal :(

--- MetalParticles/Particles.metal
+++ MetalParticles/Particles.metal
@@ -10,63 +10,35 @@
 #include <metal_stdlib>
 using namespace metal;
 
-struct Particle
+struct PrimeNumber
 {
-    float positionX;
-    float positionY;
-    float velocityX;
-    float velocityY;
+    float numericValue;
+    float isPrime;
 };
 
-kernel void glowShader(texture2d<float, access::read> inTexture [[texture(0)]],
-                       texture2d<float, access::write> outTexture [[texture(1)]],
-                       texture2d<float, access::read> inTextureB [[texture(2)]],
-                       uint2 gid [[thread_position_in_grid]])
-{
-    float4 accumColor(0,0,0,0);
-    
-    for (int j = -2; j <= 2; j++)
-    {
-        for (int i = -2; i <= 2; i++)
-        {
-            uint2 kernelIndex(gid.x + i, gid.y + j);
-            accumColor += inTexture.read(kernelIndex).rgba;
-        }
-    }
-    
-    accumColor.rgb = (accumColor.rgb / 27.0f) + inTextureB.read(gid).rgb;
-    accumColor.a = 1.0f;
-    
-    outTexture.write(accumColor, gid);
-}
 
-kernel void particleRendererShader(texture2d<float, access::write> outTexture [[texture(0)]],
-                                   const device Particle *inParticle [[ buffer(0) ]],
-                                   device Particle *outParticle [[ buffer(1) ]],
-                                   constant Particle &inGravityWell [[ buffer(2) ]],
+kernel void particleRendererShader(const device PrimeNumber *inPrimeNumbers [[ buffer(0) ]],
+                                   device PrimeNumber *outPrimeNumbers [[ buffer(1) ]],
                                    uint id [[thread_position_in_grid]])
 {
-    const uint2 particlePosition(inParticle[id].positionX, inParticle[id].positionY);
+     int thisNumber = int(inPrimeNumbers[id].numericValue);
+     int sqrtThisNumber = int( sqrt(inPrimeNumbers[id].numericValue + 1) ) ;
     
-    const Particle thisParticle = inParticle[id];
+    bool isComposite = false;
+    int idx = 2;
     
-    const int type = id % 3;
+    while (idx <= sqrtThisNumber && !isComposite)
+    {
+        isComposite = (thisNumber % idx) == 0;
+        
+        idx++;
+    }
     
-    const float4 outColor(type == 0 ? 1.0 : 0.0 , type == 1 ? 1.0 : 0.0  , type == 2 ? 1.0 : 0.0 , 1.0);
+    outPrimeNumbers[id] = PrimeNumber
+    {
+        .numericValue = inPrimeNumbers[id].numericValue,
+        .isPrime = isComposite ? 0.0f : 1.0f
+    };
     
-    const float distanceSquared = ((thisParticle.positionX - inGravityWell.positionX) * (thisParticle.positionX - inGravityWell.positionX)) +  ((thisParticle.positionY - inGravityWell.positionY) * (thisParticle.positionY - inGravityWell.positionY));
-    const float distance = distanceSquared < 1 ? 1 : sqrt(distanceSquared);
-    
-    const float factor = (1 / distance) * (type == 0 ? 0.1 : (type == 1 ? 0.125 : 0.15));
-    
-    float newVelocityX = (thisParticle.velocityX * 0.999) + (inGravityWell.positionX - thisParticle.positionX) * factor;
-    float newVelocityY = (thisParticle.velocityY * 0.999) + (inGravityWell.positionY - thisParticle.positionY) * factor;
-    
-    outParticle[id].positionX = thisParticle.positionX + thisParticle.velocityX;
-    outParticle[id].positionY = thisParticle.positionY + thisParticle.velocityY;
-    
-    outParticle[id].velocityX = newVelocityX;
-    outParticle[id].velocityY = newVelocityY;
-    
-    outTexture.write(outColor, particlePosition);
+
 }
--- MetalParticles/ViewController.swift
+++ MetalParticles/ViewController.swift
@@ -14,22 +14,6 @@ import CoreData
 class ViewController: UIViewController
 {
     
-    let bitmapInfo = CGBitmapInfo(CGBitmapInfo.ByteOrder32Big.rawValue | CGImageAlphaInfo.PremultipliedLast.rawValue)
-    let renderingIntent = kCGRenderingIntentDefault
-    
-    let imageSide: UInt = 1280
-    let imageSize = CGSize(width: Int(1280), height: Int(1280))
-    let imageByteCount = Int(1280 * 1280 * 4)
-    
-    let bytesPerPixel = UInt(4)
-    let bitsPerComponent = UInt(8)
-    let bitsPerPixel:UInt = 32
-    let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
-    
-    let bytesPerRow = UInt(4 * 1280)
-    let providerLength = Int(1280 * 1280 * 4) * sizeof(UInt8)
-    var imageBytes = [UInt8](count: Int(1280 * 1280 * 4), repeatedValue: 0)
-    
     var kernelFunction: MTLFunction!
     var pipelineState: MTLComputePipelineState!
     var defaultLibrary: MTLLibrary! = nil
@@ -38,190 +22,109 @@ class ViewController: UIViewController
     
     let imageView =  UIImageView(frame: CGRectZero)
     let markerWidget = MarkerWidget(frame: CGRectZero)
-    
-    var region: MTLRegion!
-    var textureA: MTLTexture!
-    var textureB: MTLTexture! //
-    let blankBitmapRawData = [UInt8](count: Int(1280 * 1280 * 4), repeatedValue: 0)
-    
-    var image:UIImage!
+
     var errorFlag:Bool = false
     
-    var glow_threadGroupCount:MTLSize!
-    var glow_threadGroups: MTLSize!
-    
-    var particle_threadGroupCount:MTLSize!
-    var particle_threadGroups:MTLSize!
-    
-    let particleCount: Int = 250_000
-    var particles = [Particle]()
-    
-    var gravityWell = CGPoint(x: 640, y: 640)
-    
-    var frameStartTime = CFAbsoluteTimeGetCurrent()
+    var threadGroupCount:MTLSize!
+    var threadGroups:MTLSize!
+
+    // 10^7 | 10000000 = prime count = 664,579
+    var primeNumbers = [PrimeNumber](count: 1000000, repeatedValue: PrimeNumber(numericValue: 0, isPrime: -1))
+    var cpuPrimeNumbers = [PrimeNumber](count: 1000000, repeatedValue: PrimeNumber(numericValue: 0, isPrime: -1))
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor.blackColor()
+        for i in 0 ..< primeNumbers.count
+        {
+            primeNumbers[i].numericValue = Float(i + 1)
+            cpuPrimeNumbers[i].numericValue = Float(i + 1)
+        }
+        
+        runMetal()
+        
+        runCPU()
+    }
 
-        imageView.contentMode = UIViewContentMode.ScaleAspectFit
-
-        markerWidget.alpha = 0
     
-        view.addSubview(imageView)
-        view.addSubview(markerWidget)
+    final func runCPU()
+    {
+        let frameStartTime = CFAbsoluteTimeGetCurrent()
         
-        setUpParticles()
+        let result = findPrimeNumbers()
         
+        println("----------")
+        
+        println("Swift solve time: frametime:" + NSString(format: "%.6f", CFAbsoluteTimeGetCurrent() - frameStartTime))
+        println("Swift found \(result.filter({$0.isPrime == 1}).count) prime numbers");
+    }
+
+    final func findPrimeNumbers() -> [PrimeNumber]
+    {
+       var resultdata =  [PrimeNumber](count:cpuPrimeNumbers.count, repeatedValue: PrimeNumber(numericValue: 0, isPrime: -1))
+        
+        for (id: Int, primeNumber: PrimeNumber) in enumerate(cpuPrimeNumbers)
+        {
+            let thisNumber = Int(primeNumber.numericValue)
+            let sqrtThisNumber = Int(sqrt(primeNumber.numericValue + 1))
+            
+            var isPrime = true
+            
+            for (var i = 2; i <= sqrtThisNumber; i++)
+            {
+                if (thisNumber % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            
+            resultdata[id].numericValue = Float(thisNumber);
+            resultdata[id].isPrime =  isPrime ? 1 : 0;
+            
+        }
+        
+        return resultdata
+    }
+    
+    //-------
+
+    final func runMetal()
+    {
         setUpMetal()
-    }
-    
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent)
-    {
-        let location = event.allTouches()?.anyObject()?.locationInView(imageView)
         
-        if let _location = location
-        {
-            positionGravityWell(_location)
-        }
-    }
-    
-    override func touchesMoved(touches: NSSet, withEvent event: UIEvent)
-    {
-        let location = event.allTouches()?.anyObject()?.locationInView(imageView)
+        let frameStartTime = CFAbsoluteTimeGetCurrent()
+
+        let result = metalFindPrimeNumbers()
+ 
+        println("----------")
         
-        if let _location = location
-        {
-            positionGravityWell(_location)
-        }
+        println("Metal solve time: frametime:" + NSString(format: "%.6f", CFAbsoluteTimeGetCurrent() - frameStartTime))
+        println("Metal found \(result.filter({$0.isPrime == 1 }).count) prime numbers");
     }
-    
-    func positionGravityWell(location: CGPoint)
-    {
-        if markerWidget.alpha == 0
-        {
-            UIView.animateWithDuration(0.25, animations: {self.markerWidget.alpha = 1})
-        }
-        
-        let imageScale = imageView.frame.width / CGFloat(imageSide)
-        
-        gravityWell.x = location.x / imageScale
-        gravityWell.y = location.y / imageScale
-        
-        markerWidget.frame = CGRect(x: imageView.frame.origin.x + location.x, y: imageView.frame.origin.y + location.y, width: 0, height: 0)
-    }
-    
-    override func touchesEnded(touches: NSSet, withEvent event: UIEvent)
-    {
-        UIView.animateWithDuration(1.0, delay: 2.0, options: nil, animations: {self.markerWidget.alpha = 0}, completion: nil)
-    }
-    
-    func setUpParticles()
-    {
-        for _ in 0 ..< particleCount
-        {
-            var positionX = Float(arc4random() % UInt32(imageSide))
-            var positionY = Float(arc4random() % UInt32(imageSide))
-            let velocityX = (Float(arc4random() % 10) - 5) / 10.0
-            let velocityY = (Float(arc4random() % 10) - 5) / 10.0
-         
-            let positionRule = Int(arc4random() % 4)
-            
-            if positionRule == 0
-            {
-                positionX = 0
-            }
-            else if positionRule == 1
-            {
-                positionX = Float(imageSide)
-            }
-            else if positionRule == 2
-            {
-                positionY = 0
-            }
-            else
-            {
-                positionY = Float(imageSide)
-            }
-            
-            let particle = Particle(positionX: positionX, positionY: positionY, velocityX: velocityX, velocityY: velocityY)
-    
-            particles.append(particle)
-        }
-    }
-    
-    func setUpMetal()
+
+    final func setUpMetal()
     {
         device = MTLCreateSystemDefaultDevice()
- 
+        
         if device == nil
         {
             errorFlag = true
+            return
         }
         else
         {
             defaultLibrary = device.newDefaultLibrary()
             commandQueue = device.newCommandQueue()
-   
-            particle_threadGroupCount = MTLSize(width:32,height:1,depth:1)
-            particle_threadGroups = MTLSize(width:(particles.count + 31) / 32, height:1, depth:1)
             
-            glow_threadGroupCount = MTLSizeMake(16, 16, 1)
-            glow_threadGroups = MTLSizeMake(Int(imageSide) / glow_threadGroupCount.width, Int(imageSide) / glow_threadGroupCount.height, 1)
-            
-            setUpTexture()
-            
-            run()
-        }
-    }
-
-    final func run()
-    {
-        println("frametime:" + NSString(format: "%.6f", CFAbsoluteTimeGetCurrent() - frameStartTime))
-        frameStartTime = CFAbsoluteTimeGetCurrent()
-        
-        Async.background()
-        {
-            self.applyShader()
-        }
-        .main
-        {
-            self.imageView.image = self.image
-
-            self.run();
+            threadGroupCount = MTLSize(width:32,height:1,depth:1)
+            threadGroups = MTLSize(width:(primeNumbers.count + 31) / 32, height:1, depth:1)
         }
     }
     
-    final func glowTexture()
+    final func metalFindPrimeNumbers() -> [PrimeNumber]
     {
-        commandQueue = device.newCommandQueue()
-        
-        kernelFunction = defaultLibrary.newFunctionWithName("glowShader")
-        pipelineState = device.newComputePipelineStateWithFunction(kernelFunction!, error: nil)
-        
-        let commandBuffer = commandQueue.commandBuffer()
-        let commandEncoder = commandBuffer.computeCommandEncoder()
-        
-        commandEncoder.setComputePipelineState(pipelineState)
-        
-        commandEncoder.setTexture(textureA, atIndex: 0)
-        commandEncoder.setTexture(textureA, atIndex: 1)
-        commandEncoder.setTexture(textureB, atIndex: 2)
-        
-        commandEncoder.dispatchThreadgroups(glow_threadGroups, threadsPerThreadgroup: glow_threadGroupCount)
-        
-        commandEncoder.endEncoding()
-        commandBuffer.commit()
-        commandBuffer.waitUntilCompleted()
-
-    }
-    
-    final func applyShader()
-    {
-        textureB.replaceRegion(self.region, mipmapLevel: 0, withBytes: blankBitmapRawData, bytesPerRow: Int(bytesPerRow))
-
         kernelFunction = defaultLibrary.newFunctionWithName("particleRendererShader")
         pipelineState = device.newComputePipelineStateWithFunction(kernelFunction!, error: nil)
 
@@ -230,76 +133,36 @@ class ViewController: UIViewController
         
         commandEncoder.setComputePipelineState(pipelineState)
         
-        let particleVectorByteLength = particles.count*sizeofValue(particles[0])
+        let particleVectorByteLength = primeNumbers.count * sizeofValue(primeNumbers[0])
         
-        var buffer: MTLBuffer = device.newBufferWithBytes(&particles, length: particleVectorByteLength, options: nil)
+        var buffer: MTLBuffer = device.newBufferWithBytes(&primeNumbers, length: particleVectorByteLength, options: nil)
         commandEncoder.setBuffer(buffer, offset: 0, atIndex: 0)
         
-        var inVectorBuffer = device.newBufferWithBytes(&particles, length: particleVectorByteLength, options: nil)
+        var inVectorBuffer = device.newBufferWithBytes(&primeNumbers, length: particleVectorByteLength, options: nil)
         commandEncoder.setBuffer(inVectorBuffer, offset: 0, atIndex: 0)
  
-        var resultdata =  [Particle](count:particles.count, repeatedValue: Particle(positionX: 0, positionY: 0, velocityX: 0, velocityY: 0))
+        var resultdata = [PrimeNumber](count:primeNumbers.count, repeatedValue: PrimeNumber(numericValue: 0, isPrime: -1))
+
         var outVectorBuffer = device.newBufferWithBytes(&resultdata, length: particleVectorByteLength, options: nil)
         commandEncoder.setBuffer(outVectorBuffer, offset: 0, atIndex: 1)
       
-        var gravityWellParticle = Particle(positionX: Float(gravityWell.x), positionY: Float(gravityWell.y), velocityX: 0, velocityY: 0)
-        
-        var inGravityWell = device.newBufferWithBytes(&gravityWellParticle, length: sizeofValue(gravityWellParticle), options: nil)
-        commandEncoder.setBuffer(inGravityWell, offset: 0, atIndex: 2)
-  
-        commandEncoder.setTexture(textureB, atIndex: 0)
-        
-        commandEncoder.dispatchThreadgroups(particle_threadGroups, threadsPerThreadgroup: particle_threadGroupCount)
+        commandEncoder.dispatchThreadgroups(threadGroups, threadsPerThreadgroup: threadGroupCount)
         
         commandEncoder.endEncoding()
         commandBuffer.commit()
         commandBuffer.waitUntilCompleted()
-        
-        glowTexture()
-        
+       
         var data = NSData(bytesNoCopy: outVectorBuffer.contents(),
-            length: particles.count*sizeof(Particle), freeWhenDone: false)
+        length: primeNumbers.count*sizeof(PrimeNumber), freeWhenDone: false)
 
-        data.getBytes(&particles, length:particles.count * sizeof(Particle))
+        var finalResultArray = [PrimeNumber](count: primeNumbers.count, repeatedValue: PrimeNumber(numericValue: 0, isPrime: -1))
+        
+        data.getBytes(&finalResultArray, length:primeNumbers.count * sizeof(PrimeNumber))
     
-        textureA.getBytes(&imageBytes, bytesPerRow: Int(bytesPerRow), fromRegion: region, mipmapLevel: 0)
-        
-        let providerRef = CGDataProviderCreateWithCFData(NSData(bytes: &imageBytes, length: providerLength))
-        
-        let imageRef = CGImageCreate(UInt(imageSize.width), UInt(imageSize.height), bitsPerComponent, bitsPerPixel, bytesPerRow, rgbColorSpace, bitmapInfo, providerRef, nil, false, renderingIntent)
-        
-        image =  UIImage(CGImage: imageRef)!
-    }
-
-    func setUpTexture()
-    {
-        let textureDescriptor = MTLTextureDescriptor.texture2DDescriptorWithPixelFormat(MTLPixelFormat.RGBA8Unorm, width: Int(imageSide), height: Int(imageSide), mipmapped: false)
-        
-        textureA = device.newTextureWithDescriptor(textureDescriptor)
-        textureB = device.newTextureWithDescriptor(textureDescriptor)
-        
-       region = MTLRegionMake2D(0, 0, Int(imageSide), Int(imageSide))
+        return finalResultArray
     }
 
 
-    override func viewDidLayoutSubviews()
-    {
-        if view.frame.height > view.frame.width
-        {
-           let imageSide = view.frame.width
-            
-           imageView.frame = CGRect(x: 0, y: view.frame.height / 2.0 - imageSide / 2, width: imageSide, height: imageSide).rectByInsetting(dx: -1, dy: 01)
-        }
-        else
-        {
-            let imageSide = view.frame.height
-            
-            imageView.frame = CGRect(x: view.frame.width / 2.0 - imageSide / 2 , y: 0, width: imageSide, height: imageSide).rectByInsetting(dx: -1, dy: -1)
-        }
-        
-        
-    }
-    
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
@@ -308,11 +171,14 @@ class ViewController: UIViewController
 
 }
 
-struct Particle
+struct PrimeNumber
 {
-    var positionX: Float = 0
-    var positionY: Float = 0
-    var velocityX: Float = 0
-    var velocityY: Float = 0
+    // booleans didn't work well between swift and metal, so I used a float, isPrime:
+    //  -1 = don't know
+    //  1.0 = is a prime
+    //  0.0 = is a composite
+    
+    var numericValue: Float = 0
+    var isPrime: Float = -1
 }
 
